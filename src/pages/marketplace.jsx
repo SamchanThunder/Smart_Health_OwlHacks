116c813b97663a4c9b99cd45386050c6456b1078
Folashayo <folashayo@ce-ee-e0-4d-c7-2a.wireless.temple.edu>
2024-10-05 11:30:39 -0400

    start

---
+++ src/pages/marketplace.jsx
@@ -0,0 +1,10 @@
+import '../style_sheets/Marketplace.css';
+
+export function Marketplace(){
+    return(
+        <div>
+            <h2 className='title'>Test</h2>
+        
+        </div>
+    );
+}
