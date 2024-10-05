141b28ebec9b31f9fb3c508ad958f8aaa3ba7363
SamchanThunder <samchan.lee@gmail.com>
2024-10-05 13:39:48 -0400

    Update Homepage

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,8 +1,18 @@
 import '../style_sheets/StartPage.css';
+import logo from '../images/logo.png';
 export function StartPage(){
     return(
         <div className="startPageContent">
-            Start Page
+            <div className="Compare">
+                <div id="SubCompare">
+                    <img id="LogoImg" src={logo}></img>
+                    <div id="SubText">Affordable Healthcare at your Fingertips</div>
+                    <div id="CompareButtonContainer">
+                        <button id="CButton1">Start Now</button>
+                        <button id="CButton2">Quick Compare</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
